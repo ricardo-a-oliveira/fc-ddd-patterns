90ee274c33adc3e1f2d9811dc54ba215c533617e
Ricardo Oliveira <ricardo-a-oliveira@outlook.com>
2023-03-02 00:07:06 -0300

    first customer

---
+++ ddd/modelagem-e-patterns/src/customer.ts
@@ -0,0 +1,49 @@
+class Customer {
+    private _id: string;
+    private _name: string = "";
+    private _address: string = "";
+    private _active: boolean = false;
+
+
+    constructor(id: string, name: string) {
+        this._id = id;
+        this._name = name;
+        this.validate();
+      }
+
+    
+    get id(): string {
+        return this._id;
+      }
+    
+      get name(): string {
+        return this._name;
+      }
+
+    validate() {
+        if (this._id.length === 0) {
+          throw new Error("Id is required");
+        }
+        if (this._name.length === 0) {
+          throw new Error("Name is required");
+        }
+      }
+    
+      changeName(name: string) {
+        this._name = name;
+        this.validate();
+      }
+
+    
+  activate() {
+    if (this._address === undefined) {
+      throw new Error("Address is mandatory to activate a customer");
+    }
+    this._active = true;
+  }
+
+  deactivate() {
+    this._active = false;
+  }
+    
+}
